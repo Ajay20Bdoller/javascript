7b48a691e8253b04bac5887d382c1e4a3842c4bf
Ajay20Bdoller <ajay847303@gmail.com>
2025-01-06 16:04:31 +0000

    datatype summary

---
+++ Basic/datatypes-summary.js
@@ -0,0 +1,68 @@
+//primitive 
+
+//7 types : String ,Number, Boolean , Null, Undefined , Symbol, BigInt
+
+
+
+
+
+
+const score = 100
+const scoreValue = 100.3
+//they both r no.
+
+
+const isLoggedIn = false
+const outsideTemp =null
+
+let userEmail;
+
+
+const id= Symbol('123')
+const anotherId = Symbol('123')
+ 
+// console.log(anotherId === id);
+
+
+const bigNumber = 2334444444412435n 
+
+
+
+
+
+/*
+
+js ke under language definde nhi karte hai
+js static type hai ya dynamic type hai???????
+*/
+
+
+
+
+//Reference(Non primitive)
+
+
+// Array, Objects, Functions
+
+
+
+const heros = ["shaktiman","karthi jay", "kds"]
+
+let myObj = {
+name: "ajay",
+age:" 21"
+}
+
+
+
+const myFunction = function(){
+console.log("hello world");
+
+}
+
+
+console.log(typeof null);
+
+
+
+
